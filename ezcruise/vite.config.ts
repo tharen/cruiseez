72c7a5ffab340033760468b252b46cb9047384d7
Tod Haren <104120984+tharen-odf@users.noreply.github.com>
2026-04-10 10:49:54 -0700

    Change base path in Vite config to '/ezcruise/'

--- ezcruise/vite.config.ts
+++ ezcruise/vite.config.ts
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/ezcruise/',
   plugins: [
     vue(),
     VitePWA({ registerType: 'autoUpdate' })
