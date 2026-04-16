05e9ce7bb16d0f79ecfe11281989a7808a47bbac
Tod Haren <104120984+tharen-odf@users.noreply.github.com>
2026-04-16 03:55:54 +0000

    Fixup the PWA implementation

--- ezcruise/src/main.ts
+++ ezcruise/src/main.ts
@@ -2,16 +2,4 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/sw.js')
-          .then(registration => {
-              console.log('SW registered:', registration);
-          })
-          .catch(error => {
-              console.error('SW registration failed:', error);
-          });
-  });
-}
-
 createApp(App).mount('#app')
