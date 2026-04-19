d1bbd21a5a8d6a250e49fe5b872687b2c32c5561
Tod Haren <104120984+tharen-odf@users.noreply.github.com>
2026-04-18 22:20:12 -0700

    Fix app assignment logic in vite.config.ts

--- ezcruise/vite.config.ts
+++ ezcruise/vite.config.ts
@@ -8,10 +8,11 @@ export default defineConfig(({ command }) => {
   const major_ver = pkg.version.split('.')[0];
   const minor_ver = pkg.version.split('.')[1];
   const patch_ver = pkg.version.split('.')[2];
-  const app = command === 'build' ? 'app' : 'dev';
+  // const app = command === 'build' ? 'app' : 'dev';
   // If this is GitHub pages CI build then the root needs to be adjusted
+  const app = process.env.GITHUB_REF_NAME ? 'main' ? 'app' : 'dev';
   const root = process.env.GITHUB_ACTIONS ? `/ezcruise/timber/v${major_ver}/${app}/` : `/${app}/`;
-
+  
   return {
     base: root,
     plugins: [
